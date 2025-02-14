3ed8f0c35984e3643f7f625c0d7b9b6a952bc9e2
Peter King <59886014+pmpk20@users.noreply.github.com>
2025-02-14 14:54:51 +0000

    14/02: Var sign test and plot.

--- CVScripts/XX_Microplastics_SignTest.R
+++ CVScripts/XX_Microplastics_SignTest.R
@@ -390,7 +390,7 @@ Data$Income_Quartile <-
 
 
 # *****************************
-# Section 2: Data Setup ####
+# PLOT MEAN VS VARIANCE BY INCOME ####
 # *****************************
 
 
@@ -419,7 +419,7 @@ custom_labeller <- as_labeller(
 )
 
 
-Figure_X_2 <- Data[, c("Mean_Change", 
+Figure_2 <- Data[, c("Mean_Change", 
                        "Uncertainty",
                        "EOP",
                        "Income_Quartile")] %>%
@@ -499,14 +499,130 @@ Figure_X_2 <- Data[, c("Mean_Change",
 
 ## Export and save in the right location
 ggsave(
-  Figure_X_2,
+  Figure_2,
   device = "png",
-  filename = here("CVOutput", "FigureX_Smooth_Income_EOP_TEST.png"),
+  filename = here("CVOutput", "Figure_2_Smooth_Income_EOP_TEST.png"),
   width = 25,
   height = 15,
   units = "cm",
   dpi = 500
 )
+
+
+# *****************************
+# NEW PLOT MEAN VS INCOME BY VARIANCE ####
+# *****************************
+
+
+
+## New approach to avoid deprecation issues
+custom_labeller_2 <- as_labeller(
+  c(
+    "1" = "**A**: Quartile 1 (Lowest Income)",
+    "2" = "**B**: Quartile 2",
+    "3" = "**C**: Quartile 3",
+    "4" = "**D**: Quartile 4 (Highest Income)"
+  )
+)
+
+
+Figure_2B <- Data[, c("Mean_Change", 
+                       "Uncertainty",
+                       "EOP",
+                       "Income_Quartile")] %>%
+  
+  arrange(Uncertainty) %>% 
+  
+  mutate(Variance = factor(Uncertainty, levels = unique(Uncertainty)),
+         Income_Quartile = as.factor(Income_Quartile)) %>% 
+  
+  ggplot(aes(y = EOP, 
+             x = Mean_Change, 
+             colour = Variance, 
+             fill = Variance)) +  
+  
+  # Smooth curves with alpha for SE shading
+  stat_smooth(aes(fill = Variance, 
+                  colour = Variance), 
+              alpha = 0.25,  
+              linewidth = 1.25) +
+  
+  theme_bw() +
+  
+  facet_wrap( ~ Income_Quartile,
+              nrow = 2,
+              ncol = 2,
+              labeller = custom_labeller_2
+  ) + 
+  
+  # Axes and Labels
+  ylab("Expected option prices") +
+  
+  # Add red reference lines
+  geom_vline(xintercept = 0, linetype = 'dotted', col = 'red') +
+  geom_hline(yintercept = 0, linetype = 'dotted', col = 'red') +
+  
+  scale_x_continuous(name = "Mean expected harmfulness\n[Better (-10), Worse (+10)]", limits = c(-10, 10)) + 
+  scale_y_continuous(breaks = seq(-500, 1500, 250)) +
+  
+  # Colours and fills for Income Quintile
+  scale_colour_manual(
+    name = "Income Quartile",  # Legend title for clarity
+    values = c("black", "blue", "#008080", "skyblue"),  # Custom colours
+    labels = c(
+      "Highly certain: +/- zero points (N = 151)",
+      "Mostly certain: +/- one point (N = 995)",
+      "Mostly uncertain: (+/- three points (N = 307)",
+      "Highly uncertain: (+/- five points (N = 111)"
+    )) +
+  scale_fill_manual(
+    name = "Income Quartile",  # Ensure legend aligns with colour scale
+    values = c("black", "blue", "#008080",  "skyblue"),
+    labels = c(
+      "Highly certain: +/- zero points (N = 151)",
+      "Mostly certain: +/- one point (N = 995)",
+      "Mostly uncertain: (+/- three points (N = 307)",
+      "Highly uncertain: (+/- five points (N = 111)"
+    )) +
+  
+  guides(colour = guide_legend(title = "Income Quartile", 
+                               nrow = 2, 
+                               ncol = 2))+
+  
+  theme(
+    strip.background = element_rect(fill = "white"),
+    strip.text = element_markdown(size = TextSetup$size, 
+                                  colour = TextSetup$colour, 
+                                  family = TextSetup$family),
+    legend.position = "bottom",
+    legend.text = TextSetup,
+    legend.background = element_blank(),
+    panel.grid.major.x = element_blank(),
+    panel.grid.minor.x = element_blank(),
+    panel.grid.major.y = element_blank(),
+    panel.grid.minor.y = element_blank(),
+    axis.text.x = TextSetup,
+    axis.title.x = TextSetup,
+    ## Change text to be clearer for reader
+    axis.text.y = TextSetup,
+    axis.title.y = TextSetup,
+    legend.title = TextSetup
+  ) +
+  coord_cartesian(ylim = c(-500, 1000))
+
+
+
+## Export and save in the right location
+ggsave(
+  Figure_2B,
+  device = "png",
+  filename = here("CVOutput", "Figure_2B_Smooth_Income_EOP_TEST.png"),
+  width = 25,
+  height = 15,
+  units = "cm",
+  dpi = 500
+)
+
 
 
 
